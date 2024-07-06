f2ac5f8bf8714ab3dfd6aeb781800a4fd069fa20
jayptl23 <jay@example.com>
2024-07-06 14:04:00 -0400

    edit Technique type to use Step type and enum StepType

--- types.ts
+++ types.ts
@@ -1,8 +1,19 @@
-type Technique = {
+export enum StepType {
+  inhale = "Inhale",
+  hold = "Hold",
+  exhale = "Exhale",
+}
+
+type Step = {
+  type: StepType;
+  duration: number;
+};
+
+export type Technique = {
   id: number;
   name: string;
   description: string;
   inhaleTime: number;
-  holdTime: number;
   exhaleTime: number;
+  steps: Step[];
 };
