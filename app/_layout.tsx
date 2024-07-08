8e6302b8c0527e487ab5dfd9e83bb86481f54243
jayptl23 <jay@example.com>
2024-07-08 17:08:51 -0400

    change navigation name to Home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,10 @@ const RootLayout = () => {
   return (
     <SettingsProvider>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false, title: "Home" }}
+        />
       </Stack>
     </SettingsProvider>
   );
